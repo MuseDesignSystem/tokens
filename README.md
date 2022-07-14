4309fcfac4de7e8e4a3fcd0ef3c7ab2e24d7c8c2
Dan Hannigan <dahannig@cisco.com>
2022-07-14 13:56:11 -0600

    chore: initial setup of repo for POC and testing

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# tokens
+# Muse Design System Tokens
+
+@muse-design-system/tokens
+
+## Install
+
+```sh
+yarn add @muse-design-system/tokens
+```
+
+## Usage
+
+### CSS
+
+```css
+@import "@muse-design-system/tokens/dist/tokens.css";
+```
+
+## Contributing
+
+### Submitting Issues
+todo: write this out
+### Submitting Feature Requests
+
+todo: write this out
---
+++ config.js
@@ -0,0 +1,53 @@
+/* eslint-disable sort-keys */
+
+module.exports = {
+  source: [`src/**/*.json`],
+  platforms: {
+    css: {
+      transformGroup: `custom/css`,
+      buildPath: `dist/web/`,
+      files: [
+        {
+          destination: `tokens.css`,
+          format: `css/variables`,
+          options: {
+            outputReferences: true,
+          },
+        },
+      ],
+    },
+    es6: {
+      transformGroup: `custom/es6`,
+      buildPath: `dist/web/`,
+      files: [
+        {
+          destination: `tokens.js`,
+          format: `javascript/es6`,
+        },
+      ],
+    },
+    json: {
+      transformGroup: `custom/json`,
+      buildPath: `dist/web/`,
+      files: [
+        {
+          destination: `tokens.json`,
+          format: `json`,
+        },
+      ],
+    },
+    scss: {
+      transformGroup: `custom/scss`,
+      buildPath: `dist/web/`,
+      files: [
+        {
+          destination: `tokens.scss`,
+          format: `scss/variables`,
+          options: {
+            outputReferences: true,
+          },
+        },
+      ],
+    },
+  },
+}
